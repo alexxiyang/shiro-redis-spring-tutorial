6430691d84d889cea34028138758feb37eea35fa
AlexYang <alexxiyang@gmail.com>
2018-02-12 01:38:25 +0800

    initial commit

---
+++ src/main/java/org/crazycake/shiroredis/ExampleRealm.java
@@ -0,0 +1,50 @@
+package org.crazycake.shiroredis;
+
+import org.apache.shiro.authc.*;
+import org.apache.shiro.authc.credential.SimpleCredentialsMatcher;
+import org.apache.shiro.authz.AuthorizationInfo;
+import org.apache.shiro.authz.SimpleAuthorizationInfo;
+import org.apache.shiro.realm.AuthorizingRealm;
+import org.apache.shiro.subject.PrincipalCollection;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * This realm is only for tutorial
+ * @author Alex Yang
+ *
+ */
+public class ExampleRealm extends AuthorizingRealm {
+
+	@Override
+	protected AuthorizationInfo doGetAuthorizationInfo(
+			PrincipalCollection principals) {
+		SimpleAuthorizationInfo authInfo = new SimpleAuthorizationInfo();
+		// only for tutorial
+		List<String> roles = new ArrayList<String>();
+		roles.add("schwartz");
+		authInfo.addRoles(roles);
+		return authInfo;
+	}
+
+	@Override
+	protected AuthenticationInfo doGetAuthenticationInfo(
+			AuthenticationToken token) throws AuthenticationException {
+		UsernamePasswordToken usernamePasswordToken = (UsernamePasswordToken)token;
+
+		UserInfo userInfo = new UserInfo();
+		userInfo.setUsername(usernamePasswordToken.getUsername());
+		userInfo.setAge(23);
+		userInfo.setCity("Paris");
+		// Expect password is "123456"
+		return new SimpleAuthenticationInfo(userInfo, "123456", usernamePasswordToken.getUsername());
+	}
+	
+	@PostConstruct
+	public void initCredentialsMatcher() {
+		setCredentialsMatcher(new SimpleCredentialsMatcher());
+	}
+
+}
